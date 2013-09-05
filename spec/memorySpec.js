f2e09cf73df9c8b5c75d4f492456380dc9d19209
Jerry Sievert <code@legitimatesounding.com>
2013-09-05 15:09:40 -0700

    initial import

---
+++ spec/memorySpec.js
@@ -0,0 +1,97 @@
+
+var point = { type: "Point", coordinates: [ -122, 45 ], id: "point" };
+var featureCollection = {
+  "type": "FeatureCollection",
+  "features": [{
+    "type": "Feature",
+    "id": "foo",
+    "properties": {
+      "foo": "bar"
+    },
+    "geometry":  {
+      "type": "Polygon",
+      "coordinates": [
+        [ [41.83,71.01],[56.95,33.75],[21.79,36.56],[41.83,71.01] ]
+      ]
+    }
+  }],
+  "properties": {
+    "bar": "baz"
+  }
+};
+
+describe("memory", function() {
+
+  it("should correctly add geojson as a point", function () {
+    var store = new Terraformer.GeoStore.Memory();
+
+    var spy = jasmine.createSpy();
+    store.add(point, spy);
+
+    expect(spy).toHaveBeenCalledWith(null, point);
+  });
+  
+  it("should correctly update geojson as a point", function () {
+    var store = new Terraformer.GeoStore.Memory();
+
+    var spy = jasmine.createSpy();
+    store.update(point, spy);
+
+    expect(spy).toHaveBeenCalledWith(null, point);
+  });
+
+  it("should correctly add geojson as a feature collection", function () {
+    var store = new Terraformer.GeoStore.Memory();
+
+    var spy = jasmine.createSpy();
+    store.add(featureCollection, spy);
+
+    expect(spy).toHaveBeenCalledWith(null, featureCollection);
+  });
+
+  it("should correctly get a geojson from the store", function () {
+    var store = new Terraformer.GeoStore.Memory();
+
+    var spy = jasmine.createSpy();
+    store.add(point, function (err, geojson) {
+      expect(err).toBeNull();
+      expect(geojson).toEqual(point);
+      store.get("point", spy);
+      expect(spy).toHaveBeenCalledWith(null, point);
+    });
+  });
+
+  it("should correctly remove a geojson from the store", function () {
+    var store = new Terraformer.GeoStore.Memory();
+
+    var spy = jasmine.createSpy();
+    store.add(point, function (err, geojson) {
+      expect(err).toBeNull();
+      expect(geojson).toEqual(point);
+      store.remove("point", function (err, id) {
+        expect(err).toBeNull();
+        expect(id).toEqual("point");
+        store.get("point", spy);
+        expect(spy).toHaveBeenCalledWith(null, undefined);
+      });
+    });
+  });
+
+  it("should correctly serialize and deserialize", function () {
+    var store = new Terraformer.GeoStore.Memory();
+    store.add(point);
+
+    store.serialize(function (err, data) {
+      store.remove("point");
+
+      expect(data).toEqual('{\"point\":{\"type\":\"Point\",\"coordinates\":[-122,45],\"id\":\"point\"}}');
+      store.deserialize(data, function () { });
+
+      var spy = jasmine.createSpy();
+      store.get("point", spy);
+      expect(spy).toHaveBeenCalledWith(null, point);
+    });
+  });
+
+
+});
